eccb06157f02af3c2eba1b0cf25b350e0d42229a
Ezhik1998 <tmovlian@gmail.com>
2020-06-19 23:16:27 +0300

    Add random background color generation

---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Color Generation',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Color _backgroundColor;
+
+  @override
+  void initState() {
+    super.initState();
+    _backgroundColor = Colors.white;
+  }
+
+  void _generateBackgroundColor() {
+    // print("Change color");
+    setState(() {
+      _backgroundColor = Color.fromRGBO(Random().nextInt(256),
+          Random().nextInt(256), Random().nextInt(256), Random().nextDouble());
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GestureDetector(
+        onTap: _generateBackgroundColor,
+        child: AnimatedContainer(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          color: _backgroundColor,
+          child: Center(
+            child: Text(
+              "Hey there",
+              style: TextStyle(
+                  fontSize: 28.0,
+                  color: _backgroundColor == Colors.black
+                      ? Colors.white
+                      : Colors.black),
+            ),
+          ),
+          duration: Duration(milliseconds: 500),
+          curve: Curves.easeInCubic,
+        ),
+      ),
+    );
+  }
+}
